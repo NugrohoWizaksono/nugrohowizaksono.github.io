c5f5f8f5a0b9c1b04360eebf110ed0fbe588f243
Nugroho <nugroho@thankgoditsmonday.id>
2017-04-20 11:39:07 +0700

    index

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <title>Nugroho Wizaksono - Web Dev</title>
+</head>
+<body >
+	<h1>WELCOME</h1>
+</body>
+</html>
